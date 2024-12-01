c199402ed577fd67aa2ed700f08ba658f6aca2dd
Anthony Tran <anthony.tran@coforma.io>
2024-12-01 15:24:17 -0600

    Added in tests for Day1

--- src/test/java/aoc/day01/Day01Test.java
+++ src/test/java/aoc/day01/Day01Test.java
@@ -3,9 +3,6 @@ package aoc.day01;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Day01Test {
@@ -13,24 +10,34 @@ public class Day01Test {
     @Test
     public void testPart1(){
         // Given
-        String input = "test";
+        String input =  "3   4\n" + //
+                        "4   3\n" + //
+                        "2   5\n" + //
+                        "1   3\n" + //
+                        "3   9\n" + //
+                        "3   3";
 
         // When
         String result = new Day01().part1(input);
 
         // Then
-        assertEquals(input, result);
+        assertEquals("11", result);
     }
 
     @Test
     public void testPart2(){
         // Given
-        String input = "test";
+        String input =  "3   4\n" +
+                        "4   3\n" +
+                        "2   5\n" +
+                        "1   3\n" +
+                        "3   9\n" +
+                        "3   3";
 
         // When
         String result = new Day01().part2(input);
 
         // Then
-        assertEquals(input, result);
+        assertEquals("31", result);
     }
 }
