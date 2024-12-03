14b886061714c30fead31c5a427071b595f9a027
Anthony Tran <anthony.tran@coforma.io>
2024-12-03 14:28:30 -0600

    Implemented Day 3 Part 2

--- src/main/java/aoc/day03/Day03.java
+++ src/main/java/aoc/day03/Day03.java
@@ -1,16 +1,21 @@
 package aoc.day03;
 
+import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import aoc.Day;
-import aoc.Utils;
 
 public class Day03 implements Day {
 
-    public static final Pattern INSTRUCTION = Pattern.compile("mul\\(\\d+,\\d+\\)");
-    public static final Pattern DIGIT = Pattern.compile("\\d+");
+    public static final String INSTRUCTION_REGEX = "mul\\(\\d+,\\d+\\)";
+    public static final String ENABLE_REGEX = "do\\(\\)";
+    public static final String DISABLE_REGEX = "don't\\(\\)";
+    public static final String DIGIT_REGEX = "\\d+";
+
+    public static final Pattern INSTRUCTION = Pattern.compile(INSTRUCTION_REGEX);
+    public static final Pattern DIGIT = Pattern.compile(DIGIT_REGEX);
 
     @Override
     public String part1(String input) {
@@ -29,9 +34,37 @@ public class Day03 implements Day {
         return String.valueOf(sum);
     }
 
+    // Had to convert back from streams to for loop since summing dependent on state felt more complicated 
     @Override
     public String part2(String input) {
-        return "";
+        Pattern instructionAndToggle = Pattern.compile(
+            String.format("(%s)|(%s)|(%s)", INSTRUCTION_REGEX, ENABLE_REGEX, DISABLE_REGEX)
+        );
+        Matcher matcher = instructionAndToggle.matcher(input);
+
+        boolean isEnabled = true;
+        int runningSum = 0;
+        for (MatchResult each : matcher.results().toList()) {
+            String match = each.group();
+
+            if (match.matches(ENABLE_REGEX)) {
+                isEnabled = true;
+            } else if (match.matches(DISABLE_REGEX)) {
+                isEnabled = false;
+            } else {
+                // Do not add to running sum if calculations have been toggled off
+                if (!isEnabled) {
+                    continue;
+                }
+
+                Matcher factors = DIGIT.matcher(match);
+                runningSum += factors.results()
+                                        .mapToInt(i -> Integer.parseInt(i.group()))
+                                        .reduce(1, (a, b) -> a * b);
+            }
+        }
+
+        return String.valueOf(runningSum);
     }
 
 }
--- src/test/java/aoc/day03/Day03Test.java
+++ src/test/java/aoc/day03/Day03Test.java
@@ -21,12 +21,12 @@ public class Day03Test {
     @Test
     public void testPart2(){
         // Given
-        String input =  "";
+        String input =  "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
         // When
         String result = new Day03().part2(input);
 
         // Then
-        assertEquals("not implemented", result);
+        assertEquals("48", result);
     }
 }
